7d1cb357f4f6e051fad93e64866f2f9dba9a1775
sobms2020 <sob.mixail2017@yandex.ru>
2020-03-15 19:44:42 +0300

    five and six

---
+++ parsers/parse_six.py
@@ -0,0 +1,54 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+import socks
+
+prod_list= []
+
+def get_result(url):
+    rs = requests.session()
+    rs.proxies['http'] = os.getenv("proxy", "socks5h://localhost:9150")  # что передается?
+    rs.proxies['https'] = os.getenv("proxy", "socks5h://localhost:9150")
+    rs.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0'
+    rs.headers['Accept-Language'] = 'Accept-Language: en-US,en;q=0.5'
+    result = rs.get(url, timeout=1000)
+    rs.close()
+    return result
+
+def take_list_of_products_from_page(html):
+
+    tables = html.find_all('table', class_='table1')
+    catalog = []
+    for table in tables:
+            catalog +=table.find('tbody').find_all('tr')
+    for product in catalog:
+        try:
+            prod_name = product.find_all('td')[0].text
+        except:
+            prod_name = ''
+        try:
+            prod_price = product.find_all('td')[1].text
+        except:
+            prod_price = ''
+
+        tuple = (prod_name, prod_price)
+        prod_list.append(tuple)
+
+
+# def parse_pages(page):
+#     pages = soup.find('div', class_='storefront-sorting').find('ul', class_='page-numbers').find_all('li')
+#     for element in pages:
+#         take_list_of_products_from_page(page)
+#         try:
+#             new_link = element.find('a').get('href')
+#         except:
+#             continue;
+#         page = BeautifulSoup(get_result(new_link).text, 'lxml')
+#
+#     take_list_of_products_from_page(page)
+
+base_url = 'http://tuu66yxvrnn3of7l.onion/'
+soup = BeautifulSoup(get_result(base_url).text,'lxml')
+take_list_of_products_from_page(soup)
+print(prod_list)
+print(len(prod_list))
